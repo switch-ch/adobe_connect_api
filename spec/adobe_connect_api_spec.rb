66f401155937fa5fb143086411d91ee989c41466
Katja Graefenhain <katja.graefenhain@switch.ch>
2013-03-19 16:55:30 +0100

    finished test for login & logout

--- spec/adobe_connect_api_spec.rb
+++ spec/adobe_connect_api_spec.rb
@@ -1,22 +1,47 @@
 require 'spec_helper'
 
+STATUS_OK = 'ok'
+NO_DATA = 'no-data'
+
 describe AdobeConnectAPI do
 
-  it 'should return correct version string' do
-    AdobeConnectAPI.version_string.should == "AdobeConnectAPI version #{AdobeConnectApi::VERSION}"
+  describe 'GemVersion' do
+    # standard test for gem version, see http://nithinbekal.com/2011/writing-ruby-gems-part-5-setting-up-rspec/
+    it 'should return correct version string' do
+      AdobeConnectAPI.version_string.should == "AdobeConnectAPI version #{AdobeConnectApi::VERSION}"
+    end
   end
 
-  it 'should login admin' do
-    interactconfig = YAML::load_file("./config/config.breeze.yml")[ENV["RAILS_ENV"]]
-    url = interactconfig["url"]
+  describe 'Login & logout' do
+    before(:each) do
+      @interactconfig = YAML::load_file("./config/config.breeze.yml")[ENV["RAILS_ENV"]]
+      url = @interactconfig["url"]
 
-    # open AdobeConnectAPI (use URL from config file)
-    @acs = AdobeConnectAPI.new(url, ENV["RAILS_ENV"], nil)
-    @acs.pointconfig=(interactconfig)
-    # login to Adobe Connect
-    res = XmlSimple.xml_in(@acs.login(), { 'KeyAttr' => 'name' })
-    status = res['status'].first['code']
-    status.should match "ok"
+      # open AdobeConnectAPI (use URL from config file)
+      @acs = AdobeConnectAPI.new(url, ENV["RAILS_ENV"], nil)
+    end
+
+    it 'should login admin' do
+      @acs.pointconfig=(@interactconfig)
+      # login to Adobe Connect
+      res = XmlSimple.xml_in(@acs.login(), { 'KeyAttr' => 'name' })
+      status = res['status'].first['code']
+      status.should match STATUS_OK
+    end
+
+    it 'should logout' do
+      res = XmlSimple.xml_in(@acs.logout(), { 'KeyAttr' => 'name' })
+      status = res['status'].first['code']
+      status.should include(STATUS_OK)
+    end
+
+    it 'should not login admin with wrong password' do
+      login = @interactconfig["username"]
+      res = XmlSimple.xml_in(@acs.login(login, 'password'), { 'KeyAttr' => 'name' })
+      status = res['status'].first['code']
+      status.should match NO_DATA
+    end
+
   end
 
 end
